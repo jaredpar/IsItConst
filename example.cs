94fb685848a522fd46eb6fc4f11ac08bd1fcdecf
Jared Parsons <jaredpparsons@gmail.com>
2021-03-02 15:39:53 -0800

    Oops

--- example.cs
+++ example.cs
@@ -8,10 +8,10 @@ public class Example {
     // Does not trigger CA1802
     static readonly string HeyRobotPattern = $@"(?:(?:hey|yo|hi(ya)?|hello|howdy)\s+)?@(?<robot>{NamePattern})\s+";
 
-    // Trigger CA1802 which proves the rule is running
+    // Does not trigger CA1802. This is a simpler version of HeyRobotPattern that is trivially understood to be const
     static readonly string HeyRobotPattern2 = $"example";
 
-    // Trigger CA1802 which proves the rule is running
+    // Trigger CA1802 which proves the rule is running in general.
     static readonly string HeyRobotPattern3 = "example";
 
 }
